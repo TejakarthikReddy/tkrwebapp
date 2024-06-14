c09fd22fba28f509b134759053a45d9785efda27
bankakarthik2002@gmail.com <bankakarthik2002@gmail.com>
2024-06-14 11:34:32 +0530

    jkjson

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace karthikwebapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
